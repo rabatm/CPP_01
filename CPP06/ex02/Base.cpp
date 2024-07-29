bf4c9de683c1140402732ab6c77706ddf6b3334e
mrabat <martin.rabat@gmail.com>
2024-07-29 22:24:58 +0000

    fix ex02

--- CPP06/ex02/Base.cpp
+++ CPP06/ex02/Base.cpp
@@ -2,6 +2,7 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <typeinfo>
 
 
 Base * generate(void)
@@ -38,19 +39,19 @@ void identify(Base* p)
 void identify(Base& p)
 {
     try {
-        A& a = dynamic_cast<A&>(p);
+        dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
         return;
     } catch (const std::bad_cast&) {}
 
     try {
-        B& b = dynamic_cast<B&>(p);
+        dynamic_cast<B&>(p);
         std::cout << "B" << std::endl;
         return;
     } catch (const std::bad_cast&) {}
 
     try {
-        C& c = dynamic_cast<C&>(p);
+        dynamic_cast<C&>(p);
         std::cout << "C" << std::endl;
         return;
     } catch (const std::bad_cast&) {}
